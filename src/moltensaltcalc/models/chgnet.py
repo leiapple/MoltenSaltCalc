2c864284782a94028400ddf75fb4dd7226cdc28c
daniisler <isler@differ.nl>
2026-06-09 15:15:53 +0200

    Bugfix WIP Stress CHGNet: Add a wrapper for CHGNet to convert the stress from full to voigt notation

--- src/moltensaltcalc/models/chgnet.py
+++ src/moltensaltcalc/models/chgnet.py
@@ -1,6 +1,38 @@
 """Implementation of the CHGNet MLIP."""
 
+import numpy as np
+
 from moltensaltcalc.registry import register_model
+
+
+class CHGNetVoigtWrapper:
+    """Wrapper for CHGNet calculator to convert stress to Voigt format."""
+
+    def __init__(self, calc):
+        """Initialize the wrapper."""
+        self.calc = calc
+
+    def calculate(self, atoms=None, properties=None, system_changes=None):
+        """Modify the calculator to convert stress to Voigt format."""
+        self.calc.calculate(atoms, properties, system_changes)
+        if "stress" in self.calc.results:
+            stress = self.calc.results["stress"]
+            # CHGNet gives (3,3) → convert to Voigt (6,)
+            stress_voigt = np.array(
+                [
+                    stress[0, 0],
+                    stress[1, 1],
+                    stress[2, 2],
+                    stress[0, 1],
+                    stress[1, 2],
+                    stress[0, 2],
+                ]
+            )
+            self.calc.results["stress"] = stress_voigt
+
+    def __getattr__(self, name):
+        """Forward all other calls to the calculator."""
+        return getattr(self.calc, name)
 
 
 @register_model(
@@ -21,5 +53,6 @@ def _build(params, device):
 
     chgnet = CHGNet.load(model_name=params.get("model_name", "0.3.0"), use_device=device)
     calc = CHGNetCalculator(chgnet, use_device=device)
+    calc = CHGNetVoigtWrapper(calc)
 
     return calc
